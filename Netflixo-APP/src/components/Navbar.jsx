dee740d5bd5bdd44f30f21d8f808afda04c33304
Pedroiga <100247785+Pedroiga@users.noreply.github.com>
2023-10-18 12:05:23 -0300

    Navbar pronta

--- Netflixo-APP/src/components/Navbar.jsx
+++ Netflixo-APP/src/components/Navbar.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom"
 import {BiSearchAlt2} from "react-icons/bi";
 import {BsFillFilePlayFill} from "react-icons/bs";
+import "./Navbar.css";
 
 const Navbar = () =>{
     return(
         <nav id="navbar">
           <h1>
-            <Link to="/"><BsFillFilePlayFill/> netflixo</Link>
+            <Link to="/"><BsFillFilePlayFill/>NETFLIXO</Link>
           </h1>
           <form >
             <input type="text" placeholder="Qual filme deseja?"/>
