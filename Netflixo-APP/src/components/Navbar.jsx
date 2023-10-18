15fd4f97b5fbb77410da3f5d452ae358323d2759
Pedroiga <100247785+Pedroiga@users.noreply.github.com>
2023-10-18 13:53:13 -0300

    Iniciando a parte de pesquisa

--- Netflixo-APP/src/components/Navbar.jsx
+++ Netflixo-APP/src/components/Navbar.jsx
@@ -1,16 +1,28 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import {BiSearchAlt2} from "react-icons/bi";
 import {BsFillFilePlayFill} from "react-icons/bs";
 import "./Navbar.css";
+import { useState } from "react";
 
 const Navbar = () =>{
+  const [search, setSearch] = useState("")
+  const navigate = useNavigate()
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    if(!search) return
+    navigate(`/search?q=${search}`)
+    setSearch("")
+  }
     return(
         <nav id="navbar">
           <h1>
             <Link to="/"><BsFillFilePlayFill/>NETFLIXO</Link>
           </h1>
-          <form >
-            <input type="text" placeholder="Qual filme deseja?"/>
+          <form onSubmit={handleSubmit}>
+            <input type="text" 
+            placeholder="Qual filme deseja?" 
+            onChange={(e) => setSearch(e.target.value)} 
+            value={search}/>
             <button type="submit">
                 <BiSearchAlt2/>
             </button>
