e2577a8eb1b40f95b8bf295cfe8694ca5246ec31
Pedroiga <100247785+Pedroiga@users.noreply.github.com>
2023-10-19 14:51:01 -0300

    Começando a filtrar

--- Netflixo-APP/src/components/Navbar.jsx
+++ Netflixo-APP/src/components/Navbar.jsx
@@ -1,33 +1,50 @@
 import { Link, useNavigate } from "react-router-dom"
 import {BiSearchAlt2} from "react-icons/bi";
 import {BsFillFilePlayFill} from "react-icons/bs";
+import { AiOutlineMenu } from "react-icons/ai";
 import "./Navbar.css";
-import { useState } from "react";
+import { useState, useRef } from "react";
+import { sidebar } from "./sidebar";
 
-const Navbar = () =>{
-  const [search, setSearch] = useState("")
-  const navigate = useNavigate()
-  const handleSubmit = (e) =>{
+function Navbar() {
+  const dropDownRef = useRef(null);
+  const [isActive, setIsActive] = useState(false)
+  const onClick = () => setIsActive(!isActive)
+  const [search, setSearch] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
     e.preventDefault();
-    if(!search) return;
+    if (!search) return;
     navigate(`/search?q=${search}`);
     setSearch("");
+    console.log(isActive)
   };
-    return(
-        <nav id="navbar">
-          <h1>
-            <Link to="/"><BsFillFilePlayFill/>NETFLIXO</Link>
-          </h1>
-          <form onSubmit={handleSubmit}>
-            <input type="text" 
-            placeholder="Qual filme deseja?" 
-            onChange={(e) => setSearch(e.target.value)} 
-            value={search}/>
-            <button type="submit">
-                <BiSearchAlt2/>
-            </button>
-          </form>
-        </nav>
-    )
+  return (
+    <nav id="navbar" >
+      <button className="but"><AiOutlineMenu className="barras" onClick={onClick}/></button>
+      <nav ref={dropDownRef} className={`menu ${isActive ? "active" : "inactive"}`}>
+      <ul>
+        <li><a href="">Ação</a></li>
+        <li><a href="">Comédia</a></li>
+        <li><a href="">Terror</a></li>
+        <li><a href="">Aventura</a></li>
+        <li><a href="">Drama</a></li>
+        <li><a href="">Ficção</a></li>
+      </ul>
+      </nav>
+      <h1>
+        <Link to="/"><BsFillFilePlayFill />NETFLIXO</Link>
+      </h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text"
+          placeholder="Qual filme deseja?"
+          onChange={(e) => setSearch(e.target.value)}
+          value={search} />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
+    </nav>
+  );
 }
 export default Navbar
--- Netflixo-APP/src/pages/Home.jsx
+++ Netflixo-APP/src/pages/Home.jsx
@@ -1,10 +1,9 @@
 import { useState, useEffect } from "react";
 import MovieCard from "../components/MovieCard";
 import "./MovieGrid.css"
-import { Link } from "react-router-dom";
 const moviesURL = import.meta.env.VITE_API
 const apiKey = import.meta.env.VITE_API_KEY
-
+import './MovieGrid.css';
 const Home = () =>{
     const [topMovies, setTopMovies] = useState([])
     const getTopRatedMovies = async (url) => {
