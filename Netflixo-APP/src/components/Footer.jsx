3c2384a4a0702bdacb7b58e7a94a0fddd426dbe2
Pedroiga <100247785+Pedroiga@users.noreply.github.com>
2023-10-28 15:46:52 -0300

    Update Footer.jsx

--- Netflixo-APP/src/components/Footer.jsx
+++ Netflixo-APP/src/components/Footer.jsx
@@ -6,10 +6,10 @@ function Footer(){
           <div className="container">
             <p>&copy; 2023 Pedro Henrique</p>
             <ul className='link center-ul'>
-                <li><a href="https://github.com/Pedroiga"><FaGithub /></a></li>
-                <li><a href="mailto:pedrohenso@outlook.com"><FaEnvelope /></a></li>
-                <li><a href="https://wa.me/5577981625185"><FaWhatsapp /></a></li>
-                <li><a href="https://www.linkedin.com/in/pedro-henrique-da-silva-oliveira-5a4536280/"><FaLinkedin /></a></li>
+                <li><a href="https://github.com/Pedroiga" target="_blank"><FaGithub /></a></li>
+                <li><a href="mailto:pedrohenso@outlook.com" target="_blank"><FaEnvelope /></a></li>
+                <li><a href="https://wa.me/5577981625185" target="_blank"><FaWhatsapp /></a></li>
+                <li><a href="https://www.linkedin.com/in/pedro-henrique-da-silva-oliveira-5a4536280/" target="_blank"><FaLinkedin /></a></li>
             </ul>
           </div>
         </footer>
