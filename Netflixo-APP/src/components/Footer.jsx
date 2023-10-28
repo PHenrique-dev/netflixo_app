4021569d044b14971e845f704dc6f073c3ce6f79
Pedroiga <100247785+Pedroiga@users.noreply.github.com>
2023-10-27 21:25:36 -0300

    Retirei o deploy

--- Netflixo-APP/src/components/Footer.jsx
+++ Netflixo-APP/src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer(){
         <footer className="footer">
           <div className="container">
             <p>&copy; 2023 Pedro Henrique</p>
-            <ul>
+            <ul className='link center-ul'>
                 <li><a href="https://github.com/Pedroiga"><FaGithub /></a></li>
                 <li><a href="mailto:pedrohenso@outlook.com"><FaEnvelope /></a></li>
                 <li><a href="https://wa.me/5577981625185"><FaWhatsapp /></a></li>
